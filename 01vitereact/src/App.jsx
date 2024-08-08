937732b98d818e6208e6b91cedd5e8da4c21f144
Kabir-Sial <sial059689@gmail.com>
2024-08-08 12:47:31 -0700

    pushing the vitereact project with tailwind css working

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //let counter = "0"
+
+let [counter, setCounter] = useState(0)
+// let [removeValue, setRemoveValue] = useState(0)
+
+let addValue = () =>{
+  setCounter(counter+1)
+}
+
+let removeValue = () =>{
+  setCounter(counter-1)
+
+}
+
+  return (
+    <div className='w-full h-screen bg-slate-800  text-white py-20 text-center font-semibold'>
+      <div className=''>
+
+        <h1 className='font-extrabold text-9xl'>Number Counter</h1>
+        <h3 className='text-8xl mt-10'>Counter is : {counter}</h3>
+        <button 
+        onClick={addValue}
+        className='mt-14 rounded-full px-6 py-4 text-4xl 
+        font-serif bg-blue-600 hover: cursor-default'>Add Value</button>
+        <button 
+        onClick={removeValue}
+        className='mt-14 mr-10 rounded-full px-6 py-4 text-4xl 
+        font-serif bg-blue-600 hover: cursor-default '>Remove Value</button>
+
+
+      </div>
+    </div>
+
+  )
+}
+
+export default App
