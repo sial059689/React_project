cb4192ec3e63cfc4e72e76a24981e1f939a16248
Kabir-Sial <sial059689@gmail.com>
2024-08-07 23:40:19 -0700

    working of 6th August 2024

--- 04passwordGenerator/src/App.jsx
+++ 04passwordGenerator/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+if (numberAllowed) str =+ "0123456789"
+if (charAllowed) str =+ "\!@#$%&*[]{}<>?/"
+let char = ""
+
+  }, [length, numberAllowed, charAllowed, setPassword])
   return (
-    <div className="bg-green-400">
-      <h1>Password Generator</h1>
+    <div>
+      <h1 className="text-white text-center text-4xl font-serif mt-4">Password Generator</h1>
+      <h2>Copy</h2>
     </div>
   );
 }
