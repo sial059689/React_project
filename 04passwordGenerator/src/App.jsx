515e0f3253665c7b512cbbb848ff1e7e2553bb67
Kabir-Sial <sial059689@gmail.com>
2024-08-15 00:14:11 +0500

    password generator working is now live, but copy button is still pending

--- 04passwordGenerator/src/App.jsx
+++ 04passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -16,30 +16,94 @@ function App() {
     if (numberAllow) str += "0123456789"
     if (charAllow) str += "{[(/\?|@#$)]}"
 
-    for (let i = 1; i < array.length; i++) {
-      let char = math.floor(math.random() * str.length + 1)
 
-      pass = str.charAt(char)
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+
+      pass += str.charAt(char)
     }
-
     setPassword(pass)
-    console.log(setPassword);
 
+  }, [length, numberAllow, charAllow])
 
-  }
+  {/* use Effect hook working starts here */ }
 
-    , [length, numberAllow, charAllow])
+  useEffect(() => {passGenerator()}, [length, numberAllow, charAllow, passGenerator])
 
   return (
-    <div>
-      <h1 className=" text-orange-300 text-center text-6xl font-serif mt-12 mb-4
-      ">Password Generator</h1>
-      <div className="w-full max-w-md mx-auto rounded-full  text-orange-300
-      px-4 py-3">
-<input type="text" value={password} 
-className="outline-none w-full px-4 py-3 rounded-full readOnly" placeholder="Enter Your Password" />
+    < div >
+      {/* Main/Container div working starts here */}
+
+      <div className="w-full max-w-xl mx-auto bg-gray-900 rounded-lg shadow-md
+       text-orange-300 px-4 py-3 my-24">
+
+        <h1 className="text-orange-400 text-center text-4xl pb-3">Password Generator</h1>
+
+
+        {/* password working starts here */}
+
+        <div className="flex shadow overflow-hidden mb-4 rounded-lg">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 rounded-none"
+            placeholder="Password"
+            readOnly
+
+          />
+          <button className="outline-none bg-blue-500 text-white px-3 py-0.5 shrink-0">Copy</button>
+
+        </div>
+        <div className="flex text-sm gap-x-2">
+
+          {/* Length range working starts here */}
+
+          <div className="flex items-center gap-x-1">
+
+            <input type="range"
+              min={6}
+              max={30}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>Length : {length}</label>
+          </div>
+
+          {/* Number Allow checkbox working */}
+
+          <div className="flex text-sm gap-x-2">
+
+            <input
+              type="checkbox"
+              defaultChecked={numberAllow}
+              id="numberInput"
+              onChange={() => setNumberAllow((prev => !prev))}
+            />
+            <label>Number</label>
+
+          </div>
+
+          {/* Char Allow checkbox working */}
+
+          <div className="flex text-sm gap-x-2">
+
+            <input
+              type="checkbox"
+              defaultChecked={charAllow}
+              id="charInput"
+              onChange={() => setCharAllow((prev => !prev))}
+            />
+            <label>Character</label>
+
+          </div>
+
+        </div>
+
       </div>
-    </div>
+
+    </div >
   );
 }
 
