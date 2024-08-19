c5991071ac2f94d3f05fb3c6b4155c63a605134f
Kabir-Sial <sial059689@gmail.com>
2024-08-20 00:26:46 +0500

    pushing the new project

---
+++ 05currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-center text-3xl text-orange-400'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
