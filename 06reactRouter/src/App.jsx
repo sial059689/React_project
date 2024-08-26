45457173932c6c73453d80dea729bb1ab1501b33
Kabir-Sial <sial059689@gmail.com>
2024-08-27 00:01:43 +0500

    another project with criousity

---
+++ 06reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='w-full h-screen bg-green-400'>
+
+     </div>
+    </>
+  )
+}
+
+export default App
