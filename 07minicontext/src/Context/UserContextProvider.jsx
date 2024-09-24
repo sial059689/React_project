10b464e74e7238b8544898a5ec36ea84de44c552
Kabir-Sial <sial059689@gmail.com>
2024-09-25 00:50:07 +0500

    trying to import the context

--- 07minicontext/src/Context/UserContextProvider.jsx
+++ 07minicontext/src/Context/UserContextProvider.jsx
@@ -1,2 +1,12 @@
-import React from "react";
+import React, { Children } from "react";
 import UserContext from "./userContext";
+
+
+const UserContextProvider = ({Children}) =>{
+return{
+    <UserContext.Provider>
+    {Children}
+    <UserContext.Provider/>
+}
+
+}
