09606d0e70bce7e96b690e715dd686eea2163d13
Kabir-Sial <sial059689@gmail.com>
2024-11-10 21:31:50 +0500

    still working on react props

---
+++ 08props/src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='flex w-full h-screen bg-slate-600'>
+      <div className='w-28 h-28 bg-white rounded m-10 p-3'>
+
+<h1>Name :</h1>
+<h1>Age :</h1>
+<h1>City :</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Card
