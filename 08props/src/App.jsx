b9620c248f4adb56593bc155e4e4328199a5166d
Kabir-Sial <sial059689@gmail.com>
2024-11-11 06:59:28 +0500

    still again working on props

--- 08props/src/App.jsx
+++ 08props/src/App.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import Card from './Components/Card'
 const App = () => {
+
+
+
+  const user = [
+    {
+      "name": "Alice Johnson",
+      "age": 28,
+      "city": "New York",
+      "profession": "Software Engineer",
+      "picture": "https://example.com/images/alice.jpg"
+    },
+    {
+      "name": "Bob Smith",
+      "age": 34,
+      "city": "Los Angeles",
+      "profession": "Graphic Designer",
+      "picture": "https://example.com/images/bob.jpg"
+    },
+    {
+      "name": "Catherine Liu",
+      "age": 26,
+      "city": "San Francisco",
+      "profession": "Data Analyst",
+      "picture": "https://example.com/images/catherine.jpg"
+    },
+    {
+      "name": "David Kim",
+      "age": 31,
+      "city": "Seattle",
+      "profession": "Project Manager",
+      "picture": "https://example.com/images/david.jpg"
+    },
+    {
+      "name": "Emma Brown",
+      "age": 29,
+      "city": "Chicago",
+      "profession": "Marketing Specialist",
+      "picture": "https://example.com/images/emma.jpg"
+    }
+  ]
+
+
+
   return (
     <div>
-<Card />
+      
+
+      {user.map(function (elem, idx) {
+        return <Card key={idx} Name={elem.name}/>
+      })}
 
     </div>
   )
--- 08props/src/Components/Card.jsx
+++ 08props/src/Components/Card.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = () => {
+
+
   return (
     <div className='flex w-full h-screen bg-slate-600'>
-      <div className='w-28 h-28 bg-white rounded m-10 p-3'>
+      <div className='w-40 h-56 bg-white rounded m-6 p-3 inline-block'>
+        <img  className='w-28 h-28 bg-white rounded mb-3' src="" alt="" />
+        <h1>Name : </h1>
+        <h1>Age :</h1>
+        <h1>City :</h1>
+      </div>
 
-<h1>Name :</h1>
-<h1>Age :</h1>
-<h1>City :</h1>
+      <div className='w-40 h-56 bg-white rounded m-6 p-3 inline-block'>
+        <img  className='w-28 h-28 bg-white rounded mb-3' src="" alt="" />
+        <h1>Name :</h1>
+        <h1>Age :</h1>
+        <h1>City :</h1>
+      </div>
+
+      <div className='w-40 h-56 bg-white rounded m-6 p-3 inline-block'>
+        <img  className='w-28 h-28 bg-white rounded mb-3' src="" alt="" />
+        <h1>Name :</h1>
+        <h1>Age :</h1>
+        <h1>City :</h1>
       </div>
     </div>
   )
