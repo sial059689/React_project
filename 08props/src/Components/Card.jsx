f97e219615e0695b0b2c7043c35a73b47db4ab38
Kabir-Sial <sial059689@gmail.com>
2024-12-17 00:37:14 +0500

    trying to pass data from json

--- 08props/src/Components/Card.jsx
+++ 08props/src/Components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='w-full h-screen bg-black'>
       <div className='w-40 h-56 bg-white rounded m-6 p-3 inline-block'>
         <img className='w-28 h-28 bg-white rounded mb-3' src="" alt="" />
-        <h1>Name : </h1>
+        <h1>Name : {}</h1>
         <h1>Age : </h1>
         <h1>City : </h1>
       </div>
