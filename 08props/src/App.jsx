ce4ce39891e66c21db88f6e58864ecd891042399
Kabir-Sial <sial059689@gmail.com>
2024-12-18 00:15:05 +0500

    eventually, i cracked the props concept

--- 08props/src/App.jsx
+++ 08props/src/App.jsx
@@ -2,16 +2,59 @@ import React from 'react'
 import Card from "./Components/Card.jsx"
 const App = () => {
 
-  const user = "mine"
+  const members = [
+    {
+      "name": "John Doe",
+      "city": "New York",
+      "age": 30,
+      "profession": "Software Engineer",
+      "profile_picture": "https://example.com/profile_pictures/john_doe.jpg"
+    },
+    {
+      "name": "Jane Smith",
+      "city": "Los Angeles",
+      "age": 28,
+      "profession": "Graphic Designer",
+      "profile_picture": "https://example.com/profile_pictures/jane_smith.jpg"
+    },
+    {
+      "name": "Michael Johnson",
+      "city": "Chicago",
+      "age": 35,
+      "profession": "Project Manager",
+      "profile_picture": "https://example.com/profile_pictures/michael_johnson.jpg"
+    },
+    {
+      "name": "Emily Davis",
+      "city": "Houston",
+      "age": 25,
+      "profession": "Data Analyst",
+      "profile_picture": "https://example.com/profile_pictures/emily_davis.jpg"
+    },
+    {
+      "name": "William Brown",
+      "city": "San Francisco",
+      "age": 40,
+      "profession": "Product Manager",
+      "profile_picture": "https://example.com/profile_pictures/william_brown.jpg"
+    }
+  ]
+
+
 
   return (
     <div className=''>
-      
-        <Card />
-        
-        
+      <div>
+        {members.map(function (elem, idx) {
+          return <Card  key={idx} name={elem.name} age={elem.age} city={elem.city}
+            prof={elem.profession} pic={elem.picture} />
+          })}
+
+          console.log("elem.pic")
+
       </div>
-   
+    </div>
+
   )
 }
 
--- 08props/src/Components/Card.jsx
+++ 08props/src/Components/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const Card = (props) => {
+  console.log(props.pic)
   return (
-    <div className='w-full h-screen bg-black'>
-      <div className='w-40 h-56 bg-white rounded m-6 p-3 inline-block'>
-        <img className='w-28 h-28 bg-white rounded mb-3' src="" alt="" />
-        <h1>Name : {}</h1>
-        <h1>Age : </h1>
-        <h1>City : </h1>
+    <div className=' inline-block m-auto  '>
+      <div className='bg-green-100  rounded border-solid border-2 border-indigo-600 mr-6  ml-12 mt-6 p-3'>
+        <img className='w-28 h-28  rounded mb-3' src="{props.pic}" alt="" />
+        <h1>Name : {props.name}</h1>
+        <h1>Age : {props.age}</h1>
+        <h1>City : {props.city}</h1>
+        <h1>Profession : {props.prof}</h1>
       </div>
     </div>
   )
