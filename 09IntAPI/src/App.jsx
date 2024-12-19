e3f40a4b732d2300f1229c99d40958d98f9bb624
Kabir-Sial <sial059689@gmail.com>
2024-12-20 00:04:00 +0500

    fetching the data through axios

--- 09IntAPI/src/App.jsx
+++ 09IntAPI/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=10")
+    console.log(response)
+  }
 
   return (
     <div className='p-10'>
-      <button className='bg-teal-300 text-white 
-      font-semibold text-2xl rounded  p-2'>Get Data</button>
+      <button onClick={getData} className='bg-teal-300 text-white 
+      font-semibold text-2xl rounded  p-2 active:m-0.5'>Get Data</button>
 
       <div className='bg-slate-950 text-white  px-5 py-4  mt-4 rounded'>helo</div>
     </div>
