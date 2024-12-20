2ae79762dd7a1febae2e1282bb5c3e077efbefff
Kabir-Sial <sial059689@gmail.com>
2024-12-21 00:14:11 +0500

    cracked the fetching the data through api

--- 09IntAPI/src/App.jsx
+++ 09IntAPI/src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState([])
+
   const getData = async () => {
-    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=10")
-    console.log(response)
+    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=05")
+    setData(response.data)
+    console.log(data[2].download_url)
   }
 
   return (
@@ -12,7 +16,15 @@ function App() {
       <button onClick={getData} className='bg-teal-300 text-white 
       font-semibold text-2xl rounded  p-2 active:m-0.5'>Get Data</button>
 
-      <div className='bg-slate-950 text-white  px-5 py-4  mt-4 rounded'>helo</div>
+      <div className='bg-slate-950 text-white  px-5 py-4  mt-4 rounded'>
+        {data.map(function (elem, idx) {
+          return <div key={idx} className='h-96 text-black flex w-full justify-between px-2 py-2 mb-4 bg-gray-50'>
+            <img src={elem.download_url} alt="" />
+            <h1>{elem.author}</h1>
+          </div>
+        })}
+
+      </div>
     </div>
   )
 }
