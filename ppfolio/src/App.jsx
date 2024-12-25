514a4ffeda0a370f43def4b86ff357a3874dc0c2
Kabir-Sial <sial059689@gmail.com>
2024-12-25 23:48:35 +0500

    scratching the personal port folio

---
+++ ppfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Page1 from './pages/page1'
+import Header from './components/Header'
+
+const App = () => {
+
+  return (
+    <div>
+      <Header />
+      <Page1 />
+    </div>
+  )
+}
+
+export default App
---
+++ ppfolio/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute w-full z-10 p-20 items-center justify-end flex'>
+        <button className=' px-6 text-xl border-2 hover:bg-red-500 py-3 rounded-2xl text-white'>Hire Me</button>
+    </div>
+  )
+}
+
+export default Header
