a5bff7ce7da3797149325e1fe13873d9d839b4f4
Kabir-Sial <sial059689@gmail.com>
2025-01-07 23:43:25 +0500

    personal port folio home page working

--- personalpf/src/components/TiltTex.jsx
+++ personalpf/src/components/TiltTex.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const TiltTex = () => {
   return (
-    <div>TiltTex</div>
+    <div>
+      <h1 className='text-4xl text-white mt-40'>Hello!</h1>
+      <h1 className='text-4xl text-white'>I'm <span className='text-8xl'>Rizwan Ali</span> </h1>
+
+      <h1 className='text-4xl text-white'>Fron End Developer</h1>
+      <h1 className='text-6xl text-white'>To Hire</h1>
+    </div>
   )
 }
 
