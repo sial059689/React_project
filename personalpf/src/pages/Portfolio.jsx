9c7f1c0d7f47f7a98d14cf6f1a80eea39492c37d
Kabir-Sial <sial059689@gmail.com>
2025-01-26 23:51:43 +0500

    home page working

--- personalpf/src/pages/Portfolio.jsx
+++ personalpf/src/pages/Portfolio.jsx
@@ -10,22 +10,28 @@ const Portfolio = () => {
                 <div className='w-1/6'>
 
                     <div>
-                        <div className='h-[30vh] bg-slate-500'></div>
+                        <div className='h-[30vh] bg-orange-700'></div>
 
 
-                        <div className='h-[70vh] bg-red-300 columns-4'>
+                        <div className='h-[70vh] bg-gray-700 flex flex-col space-y-8 items-center'>
 
-                            <Link to='/'>Home</Link>
-                            <Link to='/about'>About</Link>
-                            <Link to='/portfolio'>Portfolio</Link>
-                            <Link to='/contact'>Contact</Link>
+                         <div className='flex'>
+                         <i className="ri-home-office-line"></i>
+                         <Link to='/'>Home</Link>
+                         </div>
+                         <i className="ri-expand-horizontal-s-fill"></i>
+                                
+                                <Link to='/about'>About</Link>
+                                <Link to='/portfolio'>Portfolio</Link>
+                                <Link to='/contact'>Contact</Link>
+
                         </div>
                     </div>
 
                 </div>
 
                 {/* left side image section */}
-                <div className='w-5/6'>
+                <div className='5/6'>
 
                     <div className=' w-full h-screen'>
                         <img src={hImage} alt="" className='w-full h-screen bg-cover bg-no-repeat blur-0' />
